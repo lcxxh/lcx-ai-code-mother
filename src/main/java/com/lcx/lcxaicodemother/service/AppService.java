7c23c7ebe56523829e35236a890ebc7b627bb97f
lcx <2397481467@qq.com>
2025-09-25 12:05:50 +0800

    9.应用模块（应用生成） a.跑通完整业务流程，能够通过对话创建AI应用 b.提供SSE流式返回结构，并进行优化

--- src/main/java/com/lcx/lcxaicodemother/service/AppService.java
+++ src/main/java/com/lcx/lcxaicodemother/service/AppService.java
@@ -1,10 +1,12 @@
 package com.lcx.lcxaicodemother.service;
 
 import com.lcx.lcxaicodemother.model.dto.app.AppQueryRequest;
+import com.lcx.lcxaicodemother.model.entity.User;
 import com.lcx.lcxaicodemother.model.vo.AppVO;
 import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.lcx.lcxaicodemother.model.entity.App;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
@@ -35,4 +37,14 @@ public interface AppService extends IService<App> {
      * @return
      */
     List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 通过对话生成应用代码
+     * @param appId  应用Id
+     * @param message  提示词
+     * @param loginUser  登录用户
+     * @return
+     */
+
+    Flux<String> chatToGenCode(Long appId, String message, User loginUser);
 }
