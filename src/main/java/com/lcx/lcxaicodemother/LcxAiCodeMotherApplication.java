33abaeec3f555ecfdf046104f5c1d635015c82f7
lcx <2397481467@qq.com>
2025-09-28 18:19:04 +0800

    13.对话历史模块（对话记忆持久化） a.利用LangChain4j + Redis实现对话持久化 b.通过工厂为appId创建隔离的AI Service，并用Caffeine本地缓存优化 c.从数据库中加载对话历史，实现真正地持久化 d.优化前端“部署按钮”的标题

--- src/main/java/com/lcx/lcxaicodemother/LcxAiCodeMotherApplication.java
+++ src/main/java/com/lcx/lcxaicodemother/LcxAiCodeMotherApplication.java
@@ -1,11 +1,12 @@
 package com.lcx.lcxaicodemother;
 
+import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStoreAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 @MapperScan("com.lcx.lcxaicodemother.mapper")
+@SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 public class LcxAiCodeMotherApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/lcx/lcxaicodemother/ai/AiCodeGeneratorServiceTest.java
+++ src/test/java/com/lcx/lcxaicodemother/ai/AiCodeGeneratorServiceTest.java
@@ -16,7 +16,7 @@ class AiCodeGeneratorServiceTest {
     @Test
     void generateHtmlCode() {
 
-        HtmlCodeResult generateHtmlCode = aiCodeGeneratorService.generateHtmlCode("做一个程序员星痕的工作记录小工具");
+        HtmlCodeResult generateHtmlCode = aiCodeGeneratorService.generateHtmlCode(1,"做一个程序员星痕的工作记录小工具");
         Assertions.assertNotNull(generateHtmlCode);
     }
 
@@ -25,4 +25,16 @@ class AiCodeGeneratorServiceTest {
         MultiFileCodeResult generateMultiFileCode = aiCodeGeneratorService.generateMultiFileCode("做个程序员星痕的留言板");
         Assertions.assertNotNull(generateMultiFileCode);
     }
+    @Test
+    void testChatMemory() {
+        HtmlCodeResult result = aiCodeGeneratorService.generateHtmlCode("做个登录网站，总代码量不超过20行");
+        Assertions.assertNotNull(result);
+        result = aiCodeGeneratorService.generateHtmlCode("不要生成网站，告诉我你刚刚做了什么？");
+        Assertions.assertNotNull(result);
+        result = aiCodeGeneratorService.generateHtmlCode("做个登录网站，总代码量不超过20行");
+        Assertions.assertNotNull(result);
+        result = aiCodeGeneratorService.generateHtmlCode("不要生成网站，告诉我你刚刚做了什么？");
+        Assertions.assertNotNull(result);
+    }
+
 }
